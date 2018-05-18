519ee4cadf347e4f15a22f0b2e89926c19005485
被抛弃闷头写代码 <minnylin0000@gmail.com>
2018-05-18 14:11:10 +1000

    init repo

---
+++ ios-group/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-group
+//
+//  Created by Minny Lin on 14/5/18.
+//  Copyright © 2018 Minny Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ios-groupTests/ios_groupTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_groupTests.swift
+//  ios-groupTests
+//
+//  Created by Minny Lin on 14/5/18.
+//  Copyright © 2018 Minny Lin. All rights reserved.
+//
+
+import XCTest
+@testable import ios_group
+
+class ios_groupTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
