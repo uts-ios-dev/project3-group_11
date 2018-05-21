325b957eb15f7587f17486536dcf0151e70dab10
Chris <12711608@student.uts.edu.au>
2018-05-21 14:48:43 +1000

    protocol

---
+++ ios-group/Protocols/ProductUtilProtocols.swift
@@ -0,0 +1,20 @@
+//
+//  ProductProtocols.swift
+//  ios-group
+//
+//  Created by Christopher on 21/05/2018.
+//  Copyright © 2018 Minny Lin. All rights reserved.
+//
+
+import UIKit
+ 
+protocol ProductUtilProtocols {
+
+    func getExpireProduct()
+    func getUnExpireProduct()
+    func getPreExpireProduct()
+    func removeProductById(id:Int)
+    func getProductById(id:Int)
+    
+}
+
